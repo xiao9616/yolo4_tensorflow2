48a2644dc175ce2dd0ef4c720c4570fa19f2a9a1
18710760109 <18710760109@163.com>
2020-04-27 18:09:35 +0800

    完成yolo4主体模型的搭建，添加readme内容

--- src/yolo4/yolo4_Model.py
+++ src/yolo4/yolo4_Model.py
@@ -14,30 +14,20 @@ from tensorflow.keras.layers import ZeroPadding2D
 from tensorflow.keras.layers import Add
 from tensorflow.keras.layers import UpSampling2D
 from tensorflow.keras.layers import Concatenate
-from tensorflow.keras.layers import Activation
+from tensorflow.keras.layers import Activation, MaxPool2D, UpSampling2D
+from tensorflow.keras.activations import linear
 from .Mish import *
 
 
-def conv(x,
-         filters,
-         kernel_size,
-         strides=(1, 1),
-         padding='same',
-         data_format=None,
-         dilation_rate=(1, 1),
-         activation=None,
-         use_bias=True,
-         kernel_initializer='glorot_uniform',
-         bias_initializer='zeros',
-         kernel_regularizer=None,
-         bias_regularizer=None,
-         activity_regularizer=None,
-         kernel_constraint=None,
-         bias_constraint=None,
-         **kwargs):
+def conv(x, filters, kernel_size, strides=(1, 1), padding='same', activation="Mish", use_bias=True):
     x = Conv2D(filters, kernel_size, strides, padding, use_bias=use_bias)(x)
     x = BatchNormalization()(x)
-    x = Activation('Mish')(x)
+    if activation == 'LeakRelu':
+        x = LeakyReLU()(x)
+    if activation == 'Mish':
+        x = Activation('Mish')(x)
+    if activation == 'Linear':
+        x = linear(x)
     return x
 
 
@@ -60,10 +50,69 @@ def res_sub(x, itea, num_filters):
     return x
 
 
+def spp_sub(x):
+    x1 = MaxPool2D(strides=(1, 1), pool_size=(3, 3), padding='same')(x)
+    x2 = MaxPool2D(strides=(1, 1), pool_size=(9, 9), padding='same')(x)
+    x3 = MaxPool2D(strides=(1, 1), pool_size=(13, 13), padding='same')(x)
+    x_out = Concatenate()([x1, x2, x3, x])
+    return x_out
+
+
+def upper_concate(x1, x2, num_filter1, num_filter2):
+    x1 = conv(x1, num_filter1, (1, 1), activation='LeakRelu')
+    x2 = conv(x2, num_filter1, (1, 1), activation='LeakRelu')
+    x2 = UpSampling2D()(x2)
+    x = Concatenate()([x1, x2])
+    x = conv(x, num_filter1, (1, 1), activation='LeakRelu')
+    x = conv(x, num_filter2, (3, 3), activation='LeakRelu')
+    x = conv(x, num_filter1, (1, 1), activation='LeakRelu')
+    x = conv(x, num_filter2, (3, 3), activation='LeakRelu')
+    x = conv(x, num_filter1, (1, 1), activation='LeakRelu')
+    return x
+
+
+def spp(x):
+    x = conv(x, 512, (1, 1), activation='LeakRelu')
+    x = conv(x, 1024, (3, 3), activation='LeakRelu')
+    x = conv(x, 512, (1, 1), activation='LeakRelu')
+    x = spp_sub(x)
+    x = conv(x, 512, (1, 1), activation='LeakRelu')
+    x = conv(x, 1024, (1, 1), activation='LeakRelu')
+    x = conv(x, 512, (1, 1), activation='LeakRelu')
+    return x
+
+
+def yolo(x, num_filter1, num_filter2):
+    x = conv(x, num_filter1, (3, 3), activation='LeakRelu')
+    x = conv(x, num_filter2, (1, 1), activation='Linear')
+    return x
+
+
+def merge(x1, x2, num_filter1, num_filter2):
+    x1 = conv(x1, num_filter1, (3, 3), strides=(2, 2), activation='LeakRule')
+    x = Concatenate()([x1, x2])
+    x = conv(x, num_filter1, (1, 1), activation='LeakRelu')
+    x = conv(x, num_filter2, (3, 3), activation='LeakRelu')
+    x = conv(x, num_filter1, (1, 1), activation='LeakRelu')
+    x = conv(x, num_filter2, (3, 3), activation='LeakRelu')
+    x = conv(x, num_filter1, (1, 1), activation='LeakRelu')
+
+    return x
+
+
 def yolo4(input):
     x = conv(input, 32, (3, 3))
     x = res(x, 64, 1, 64, 64, 64, 64)
     x = res(x, 128, 2, 64, 64, 64, 128)
-    x = res(x, 128, 8, 128, 128, 128, 256)
-
-    return x
+    x1 = res(x, 128, 8, 128, 128, 128, 256)
+    x2 = res(x1, 256, 8, 256, 256, 256, 512)
+    x3 = res(x2, 1024, 4, 512, 512, 512, 1024)
+    x3 = spp(x3)
+    x2 = upper_concate(x2, x3, 256, 512)
+    x1 = upper_concate(x1, x2, 128, 256)
+    yolo1 = yolo(x1, 256, 255)
+    x2 = merge(x1, x2, 256, 512)
+    yolo2 = yolo(x2, 512, 255)
+    x3 = merge(x2, x3, 512, 1024)
+    yolo3 = yolo(x3, 1024, 255)
+    return yolo1, yolo2, yolo3
